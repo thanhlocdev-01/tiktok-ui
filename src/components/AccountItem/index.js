dbb4863e9e9d2433a9f8253af71e38d8a5a30cb1
ThanhLoc <thanhloclol6@gmail.com>
2023-03-17 15:15:07 +0700

    Header #2-3

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p9-sign-sg.tiktokcdn.com/tiktok-obj/1663218051966977~c5_300x300.webp?x-expires=1678874400&x-signature=%2FQvVay%2FpdAZvhoKfqvxBKDD0G7g%3D"
+                all="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
